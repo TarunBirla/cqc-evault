35bc9f6cf9cf68096544aba3a0e23557a62468fb
Tarun Birla <tarunbirla2018@gmail.com>
2026-01-08 10:17:06 +0530

    chnagehhdgfg

--- resources/views/admin/cqc/index.blade.php
+++ resources/views/admin/cqc/index.blade.php
@@ -1,6 +1,26 @@
 <div class="container mt-4">
 <h4>CQC E-Vault</h4>
 
+
+<div class="container mt-4">
+
+<h4>CQC E-Vault</h4>
+
+<!-- CREATE FOLDER FORM -->
+<form method="POST" action="{{ url('cqc-vault/folder/create') }}" class="mb-4">
+@csrf
+<div class="row">
+    <div class="col-md-9">
+        <input type="text" name="name" class="form-control"
+               placeholder="Enter Folder Name" required>
+    </div>
+    <div class="col-md-3">
+        <button class="btn btn-success w-100">Create Folder</button>
+    </div>
+</div>
+</form>
+
+
 <div class="row">
 @foreach($folders as $folder)
 <div class="col-md-3">
@@ -8,9 +28,10 @@
     <div class="card-body">
       <i class="bi bi-folder-fill fs-1 text-warning"></i>
       <h6>{{ $folder->name }}</h6>
-      <a href="/admin/cqc-vault/folder/{{ $folder->id }}" class="btn btn-sm btn-primary">
-        View
-      </a>
+      <a href="{{ url('cqc-vault/folder/'.$folder->id) }}"
+   class="btn btn-sm btn-primary">
+   View
+</a>
     </div>
   </div>
 </div>
